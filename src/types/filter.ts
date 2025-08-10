6807f33ddf4d2111623ad3f52dff30cbb1ddf3ed
maxim-z <maxim.zabilo@gmail.com>
2025-08-09 19:38:21 -0700

    allowing all modes for openings

--- src/types/filter.ts
+++ src/types/filter.ts
@@ -1,19 +1,19 @@
 // Used for keeping track of how songs are filtered
 export type FilterType = 
-// |   "Top 1000 by Song Viewcount"
-// |   "Random Preset 1"
-// |   "Random Preset 2"
-// |   "Random Preset 3"
+|   "Top 1000 by Song Viewcount"
+|   "Random Preset 1"
+|   "Random Preset 2"
+|   "Random Preset 3"
 |   "Top 1000 by Anime Score"
 |   "Random Preset 4"
 |   "Random Preset 5"
 |   "Random Preset 6";
 
 export const filters: FilterType[] = [
-    // "Top 1000 by Song Viewcount",
-    // "Random Preset 1",
-    // "Random Preset 2",
-    // "Random Preset 3",
+    "Top 1000 by Song Viewcount",
+    "Random Preset 1",
+    "Random Preset 2",
+    "Random Preset 3",
     "Top 1000 by Anime Score",
     "Random Preset 4",
     "Random Preset 5",
