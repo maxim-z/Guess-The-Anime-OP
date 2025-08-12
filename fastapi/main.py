e8535556adcd2e0d9152b728cf8fb738aebd234a
maxim-z <maxim.zabilo@gmail.com>
2025-08-11 18:45:18 -0700

    fixing fastapi

--- fastapi/main.py
+++ fastapi/main.py
@@ -54,7 +54,7 @@ def db_test():
 #                         score,
 #                         num_members,
 #                         genres,
-#                         studio,
+#                         studios,
 #                         id,
 #                         op_name,
 #                         yt_url,
@@ -65,7 +65,7 @@ def db_test():
 #     return [{"anime_id": row[0], "eng_title": row[1], "def_title": row[2],
 #              "rank": row[3], "year_released": row[4], "season": row[5],
 #               "num_episodes": row[6], "score": row[7], "num_members": row[8],
-#                "genres": row[9], "studio": row[10], "id": row[11],
+#                "genres": row[9], "studios": row[10], "id": row[11],
 #                 "op_name": row[12], "yt_url": row[13], "yt_viewcount": row[14]} for row in songs]
 
 # @app.get("/songs")
@@ -73,22 +73,22 @@ def db_test():
 #     return get_songs()
 
 song_filters = [
-    "Top 1000 by Song Viewcount",
+    "Top 1000 by Anime Score",
     "Random Preset 1",
     "Random Preset 2",
     "Random Preset 3",
-    "Top 1000 by Anime Score",
+    "Top 1000 by Song Viewcount",
     "Random Preset 4",
     "Random Preset 5",
     "Random Preset 6"
 ]
 
 tables = {
-    song_filters[0]: "top_1000_by_viewcount",
+    song_filters[0]: "top_1000_by_score",
     song_filters[1]: "random_presets",
     song_filters[2]: "random_presets",
     song_filters[3]: "random_presets",
-    song_filters[4]: "top_1000_by_score",
+    song_filters[4]: "top_1000_by_viewcount",
     song_filters[5]: "random_presets",
     song_filters[6]: "random_presets",
     song_filters[7]: "random_presets"
@@ -131,14 +131,14 @@ def get_from_table_top_1000_by_viewcount(id):
                         score,
                         num_members,
                         genres,
-                        studio,
+                        studios,
                         id,
                         song_title,
                         song_artist,
                         yt_video_id,
                         yt_viewcount
                    FROM top_1000_by_viewcount 
-                   LEFT JOIN anime ON anime.mal_id=top_1000_by_viewcount.anime_id LIMIT 1 OFFSET (%s)''', (str(id-1).strip(),))
+                   INNER JOIN anime ON anime.mal_id=top_1000_by_viewcount.anime_id LIMIT 1 OFFSET (%s)''', (str(id-1).strip(),))
                 #    FROM top_1000_by_viewcount WHERE id=(?)''', (str(id).strip(),))
     song = cursor.fetchone()
     cursor.close()
@@ -160,14 +160,14 @@ def get_from_table_top_1000_by_score(id):
                         score,
                         num_members,
                         genres,
-                        studio,
+                        studios,
                         id,
                         song_title,
                         song_artist,
                         yt_video_id,
                         yt_viewcount
                    FROM top_1000_by_score 
-                   LEFT JOIN anime ON anime.mal_id=top_1000_by_score.anime_id LIMIT 1 OFFSET (%s)''', (str(id-1).strip(),))
+                   INNER JOIN anime ON anime.mal_id=top_1000_by_score.anime_id LIMIT 1 OFFSET (%s)''', (str(id-1).strip(),))
                 #    FROM top_1000_by_score WHERE id=(?)''', (str(id).strip(),))
     song = cursor.fetchone()
     cursor.close()
@@ -193,7 +193,7 @@ def get_song(id, filter):
     return {"anime_id": song[0], "eng_title": song[1], "def_title": song[2],
              "rank": song[3], "year_released": song[4], "season": song[5],
               "num_episodes": song[6], "score": song[7], "num_members": song[8],
-               "genres": song[9], "studio": song[10], "id": song[11],
+               "genres": song[9], "studios": song[10], "id": song[11],
                 "song_title": song[12], "song_artist": song[13], "yt_video_id": song[14], "yt_viewcount": song[15]}
 
 @app.get("/song")
--- src/types/song.ts
+++ src/types/song.ts
@@ -22,42 +22,42 @@ export type SongFilterType =
 |  "yt_viewcount"
 
 export const song_params: SongFilterType[] = [
-    "anime_id",
-    "eng_title",
-    "def_title",
-    "rank",
-    "year_released",
-    "season",
-    "num_episodes",
-    "score",
-    "num_members",
-    "genres",
-    "studio",
-    "id",
-    "song_title",
-    "song_artist",
-    "yt_video_id",
-    "yt_viewcount"
-  ] as const;
-  
-  export type SongType = null | {
-    anime_id: number,
-    eng_title: string,
-    def_title: string,
-    rank: number,
-    year_released: number,
-    season: string,
-    num_episodes: number,
-    score: number,
-    num_members: number,
-    genres: string,
-    studio: string,
-    id: number,
-    song_title: string,
-    song_artist: string,
-    yt_video_id: string,
-    yt_viewcount: number
-  }
+  "anime_id",
+  "eng_title",
+  "def_title",
+  "rank",
+  "year_released",
+  "season",
+  "num_episodes",
+  "score",
+  "num_members",
+  "genres",
+  "studio",
+  "id",
+  "song_title",
+  "song_artist",
+  "yt_video_id",
+  "yt_viewcount"
+] as const;
+
+export type SongType = null | {
+  anime_id: number,
+  eng_title: string,
+  def_title: string,
+  rank: number,
+  year_released: number,
+  season: string,
+  num_episodes: number,
+  score: number,
+  num_members: number,
+  genres: string,
+  studio: string,
+  id: number,
+  song_title: string,
+  song_artist: string,
+  yt_video_id: string,
+  yt_viewcount: number
+}
 
 export type SongProps = {
     song_id: number;
