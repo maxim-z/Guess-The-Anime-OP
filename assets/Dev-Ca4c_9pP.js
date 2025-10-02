46e21158a1247a8a0ee26df717c73fe4c67b9f59
maxim-z <maxim.zabilo@gmail.com>
2025-10-01 19:12:22 -0700

    Updates

--- assets/Dev-Ca4c_9pP.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CXT77Np5.js";function t(){return e.jsx("div",{children:"Dev"})}export{t as default};
---
+++ assets/Dev-D4zAShOt.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-rnBg6eOI.js";function t(){return e.jsx("div",{children:"Dev"})}export{t as default};
